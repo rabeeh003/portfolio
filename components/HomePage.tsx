f919b2dc622c62cb1b8e44a8ee0f23132ae0ac5b
rabeeh003 <rabimon003@gmail.com>
2024-03-25 19:50:43 +0530

    fix some error

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -11,9 +11,9 @@ function HomePage() {
     <ScrollArea className='h-[95vh] w-[90vh] ml-3 p-6 rounded-md bg-primary-foreground shadow-md mr-3'>
       {/* banner section */}
       <div className='h-[350px] relative flex justify-end align-bottom'>
-        <img className='absolute right-0 opacity-10 w-[80%] h-[300px] rounded-md' src={"/Images/mala.jpg"} alt='bgImg1' />  
+        <Image className='absolute right-0 opacity-10 w-[80%] h-[300px] rounded-md' width={100} height={100} src={"/Images/mala2.jpg"} alt='bgImg1' />  
         <div className='absolute bottom-6 opacity-15 rounded-md right-0 h-[300px] w-[90%] bg-white border border-t-2 '></div>
-        <img className='absolute bottom-0 w-[100%] h-[300px] rounded-md' src='https://media.istockphoto.com/id/476098860/vector/wonderful-morning-in-the-blue-mountains.jpg?s=612x612&w=0&k=20&c=0nuLvsWKXPReu01RvbXTKIwlUYxOQvoXD_qVBrsapxc=' alt='bgImg1' />
+        <Image className='absolute bottom-0 w-[100%] h-[300px] rounded-md' width={100} height={100} src={"/Images/mala2.jpg"} alt='bgImg1' />
         <Image className='absolute bottom-0 left-0 w-[38%] rounded-md' src={Cutout} alt='bgImg1' />
         <h1 className='absolute top-0 font-semibold right-0 text-[2rem] font-mono [text-shadow:0_1px_0_rgba(255,_255,_255,_0.4)]' >Discover my Amazing Art Space!</h1>
       </div>
