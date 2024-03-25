84162969ff45395c790f0dc4d25df64e182cf960
rabeeh003 <rabimon003@gmail.com>
2024-03-25 20:15:26 +0530

    fix an error from about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,7 +10,7 @@ function About() {
                 {/* about section */}
                 <h3 className='text-lg font-semibold my-6'>About</h3>
                 <div className='bg-background bg-opacity-10 p-5 rounded-md'>
-                    <p>Hello! I am Muhammed Rabeeh from Malappuram, Kerala. I am a full-stack developer using Python and Django, and I am proficient in MERN stack as well. I completed my secondary education from NIOS (National Institute of Open School). I pursued my graduation in BA Arabic from Calicut University, and for the previous 8 years, I studied at Islamic College named Jamia Mahiriyya, affiliated with Jamia Nooriya. Unfortunately, I didn't complete my graduation as I focused on self-learning coding during this period. After that, I joined Brocamb.</p>
+                    <p>Hello! I am Muhammed Rabeeh from Malappuram, Kerala. I am a full-stack developer using Python and Django, and I am proficient in MERN stack as well. I completed my secondary education from NIOS (National Institute of Open School). I pursued my graduation in BA Arabic from Calicut University, and for the previous 8 years, I studied at Islamic College named Jamia Mahiriyya, affiliated with Jamia Nooriya. Unfortunately, I didn&apos;t complete my graduation as I focused on self-learning coding during this period. After that, I joined Brocamb.</p>
                 </div>
             </div>
             <Profile/>
